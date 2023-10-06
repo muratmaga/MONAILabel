2cc10172c35409785e0f835aa60a4f3320b09303
muratmaga <muratmaga@outlook.com>
2023-10-06 02:45:57 +0000

    Update monai-setup.sh

--- monai-setup.sh
+++ monai-setup.sh
@@ -5,10 +5,12 @@
 #set this path to the location of the nnunet path defined from yesterday
 BASE=/home/exouser/Downloads/nnmouse/raw/Dataset001_mouse/
 
-OUT=/home/exouser/monailabelData
+OUT=/home/exouser/MONAI/MyData
+
 mkdir -p $OUT/labels/final
 mkdir -p $OUT/labels/original
 
 
 cp $BASE/imagesTr/* $OUT
 cp $BASE/labelsTr/* $OUT/labels/final
+
